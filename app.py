f97e9709f0c28210231c045a23cb1bd662d0d794
Paulo Feltz Pinheiro <pauloupgrad@gmail.com>
2017-04-29 23:43:07 -0300

    emplementado banco de dados Sqlite3 com sqlalchemy

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from bottle import request, template
 from bottle import static_file, get
 from bottle import error
 import os
+from model import insert_user
+
 
 # static routes
 @get('/<filename:re:.*\.css>')
@@ -20,14 +22,23 @@ def images(filename):
 @get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
 def fonts(filename):
 	return static_file(filename, root='static/fonts')
-'''
-@route('/')
-def index():
-	return template('index')
-'''
+
+
 @route('/') # @get('/login') cai direto na página login
 def login():
 	return template('login')
+
+# ir para page cadastro
+@route('/cadastro')
+def cadastro():
+	return template('cadastro')	
+
+@route('/cadastro', method='POST') # @post('/cadastro')
+def acao_cadastro():
+	username = request.forms.get('username')
+	password = request.forms.get('password')
+	insert_user(username, password)	
+	return template('verificacao_cadastro', nome=username)
 
 def check_login(username, password):
 	d = {'paulo':'275293ok', 'aline':'123'}
