92e540933cf3f1fae5b4c1d4dfede2c83e2a8f34
Paulo Feltz Pinheiro <pauloupgrad@gmail.com>
2017-05-01 18:38:01 -0300

    implementando existencia de usuario

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models.tables import User
-from bottle import template, static_file, request
+from bottle import template, static_file, request, redirect
 
 # static routes
 @app.get('/<filename:re:.*\.css>')
@@ -22,26 +22,42 @@ def fonts(filename):
 
 @app.route('/') # @get('/login') cai direto na página login
 def login():
-	return template('login')
+	return template('login', sucesso=True)
 
 # ir para page cadastro
 @app.route('/cadastro')
 def cadastro():
-	return template('cadastro')	
+	return template('cadastro', existe_username=False)	
 
-@app.route('/cadastro', method='POST') # @post('/cadastro')
+@app.route('/cadastro', method='POST')
 def acao_cadastro(db):
 	username = request.forms.get('username')
 	password = request.forms.get('password')
-	new_user = User(username, password)
-	db.add(new_user)
-	return template('verificacao_cadastro', nome=username)
+	try:
+		db.query(User).filter(User.username == username).one()
+		existe_username = True
+	except:
+		existe_username = False
+	if not existe_username:
+		new_user = User(username, password)
+		db.add(new_user)
+		return redirect('/usuarios')
+	return template('cadastro', existe_username=True)
 
 @app.route('/', method='POST') # @post('/login')
-def acao_login():
+def acao_login(db):
 	username = request.forms.get('username')
 	password = request.forms.get('password')
-	return template('verificacao_login', sucesso=True)
+	result = db.query(User).filter((User.username == username) & (User.password == password)).all()
+	
+	if result:
+		return redirect('/usuarios')
+	return template('login', sucesso=False)
+
+@app.route('/usuarios')
+def usuarios(db):
+	usuarios = db.query(User).all()
+	return template('lista_usuarios', usuarios=usuarios)	
 
 @app.error(404)
 def error404(error):
